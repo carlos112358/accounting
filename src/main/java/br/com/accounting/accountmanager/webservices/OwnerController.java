025274f17da1c2d38e8b972c86b8474ef8bdb4ac
carlos112358 <cadu.on@gmail.com>
2017-01-16 00:09:20 -0200

    removed useless imports

--- src/main/java/br/com/accounting/accountmanager/webservices/OwnerController.java
+++ src/main/java/br/com/accounting/accountmanager/webservices/OwnerController.java
@@ -1,12 +1,9 @@
 package br.com.accounting.accountmanager.webservices;
 
-import br.com.accounting.accountmanager.domain.Account;
 import br.com.accounting.accountmanager.domain.Owner;
 import br.com.accounting.accountmanager.services.OwnerService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
